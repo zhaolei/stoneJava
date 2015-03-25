66a9d2bd06bb7e044a069fd06750534e2a992a3a
zhaolei <zhaolei@i1602.com>
2015-03-25 12:25:10 +0800

    new pro

---
+++ StoneJava/src/main/java/SqlA.java
@@ -0,0 +1,43 @@
+package org.stone;
+import java.sql.*;
+
+public class SqlA 
+{
+  public static void main(String[] args)
+  {
+    try
+    {
+      String url="jdbc:mysql://localhost/blog";
+      String user="root";
+      String pwd="329818";
+      
+      //加载驱动，这一句也可写为：Class.forName("com.mysql.jdbc.Driver");
+     Class.forName("com.mysql.jdbc.Driver").newInstance();
+      //建立到MySQL的连接
+       Connection conn = DriverManager.getConnection(url,user, pwd);
+      
+      //执行SQL语句
+       Statement stmt = conn.createStatement();//创建语句对象，用以执行sql语言
+      //ResultSet rs = stmt.executeQuery("select * from blog_users where id=1");
+      ResultSet rs = stmt.executeQuery("select * from blog_comments");
+     
+       //处理结果集
+      while (rs.next())
+      {
+        String name = rs.getString("comment_author_url");
+        System.out.println(name);
+      }
+       System.out.println( "1.****************************" );
+      //rs.close();//关闭数据库
+      //conn.close();
+       System.out.println( "2.****************************" );
+    }
+    catch (Exception ex)
+    {
+      System.out.println("Error : " + ex.toString());
+    }
+
+       System.out.println( "3.****************************" );
+	return;
+  }
+}
---
+++ StoneJava/src/main/java/org/stone/App.java
@@ -0,0 +1,13 @@
+package org.stone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
