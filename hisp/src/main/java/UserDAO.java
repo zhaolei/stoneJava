a138f85259e242cc2f63a8573871e63dcf6e4d33
work <work@localhost.localdomain>
2015-06-12 06:06:04 +0800

    add spring

---
+++ hisp/src/main/java/UserDAO.java
@@ -0,0 +1,11 @@
+package org.stone;  
+import org.springframework.stereotype.Component;  
+		      
+		    @Component  
+			    public class UserDAO {  
+					          
+					        public User getUser() {  
+								            return new User("qiantujava");  
+											        }  
+							          
+							    }  
---
+++ hisp/src/test/java/UserServiceTest.java
@@ -0,0 +1,22 @@
+package org.stone;  
+  
+import org.junit.Test;  
+import org.springframework.context.ApplicationContext;  
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;  
+  
+import junit.framework.TestCase;  
+  
+public class UserServiceTest extends TestCase {  
+	    @Test  
+			    public void testPrintUser() {  
+					        ApplicationContext ctx =   
+								                new AnnotationConfigApplicationContext(ApplicationConfig.class);  
+							          
+							        UserService userService = ctx.getBean(UserService.class);  
+									        userService.printUser();  
+											          
+											        //用  @Configuration + @Bean 的时候用这个获取bean  
+											//      UserService userService = (UserService) ctx.getBean("userService");  
+											          
+											    }  
+}
