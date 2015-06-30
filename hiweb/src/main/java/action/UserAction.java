ed78d8bfde625cfb78377a95f7e30e76040af43a
work <work@localhost.localdomain>
2015-06-30 19:21:16 +0800

    file ok

---
+++ hiweb/src/main/java/action/UserAction.java
@@ -0,0 +1,18 @@
+package org.stone.action;  
+  
+public class UserAction {  
+	private String username;  
+		  
+	public String getUsername() {  
+		return username;  
+	}  
+			  
+	public void setUsername(String username) {  
+		this.username = username;  
+	}  
+				      
+	public  String execute(){  
+		return "SUCCESS";  
+	}  
+					  
+}  
